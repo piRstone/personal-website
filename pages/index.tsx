06e82fe00dbf3da6516781166d95e36b28204d69
Pierre Lavalley <pierre.lavalley@gmail.com>
2021-04-23 14:12:00 +0200

    chore: setup Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>piRstone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
