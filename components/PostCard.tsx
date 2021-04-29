ce28fa6f41f13eb22c0f70aa7f6c63288d118b40
Pierre Lavalley <pierre.lavalley@gmail.com>
2021-04-29 10:38:15 +0200

    feat: categories page and category tag on post

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -11,7 +11,7 @@ type PostCardProps = {
 
 const PostCard: FC<PostCardProps> = ({ title, filePath, publishedAt, excerpt }) => {
     return (
-        <div className="mb-5 p-4 bg-gray-200 dark:bg-gray-800 rounded-md">
+        <div className="mb-5 p-6 bg-gray-100 dark:bg-gray-800 rounded-md">
             <article>
                 <Link href={`/blog/${filePath.replace(/\.mdx?$/, '')}`}>
                     <a>
--- components/PostLayout.tsx
+++ components/PostLayout.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from 'next-seo'
 import Footer from './Footer'
 import Header from './Header'
 import PostTitle from './PostTitle'
+import CategoryTag from './CategoryTag'
 import { PostMetaData } from '../types'
 
 type PostLayoutProps = {
@@ -17,11 +18,16 @@ const PostLayout: FC<PostLayoutProps> = ({ meta, content }) => {
             <div className="bg-white dark:bg-gray-900 min-h-screen">
                 <Header />
                 <main className="container mx-auto py-6">
-                    <div className="prose dark:prose-dark max-w-screen-sm mx-auto">
-                        <PostTitle meta={meta} />
-                        <p>{meta.excerpt}</p>
-                        {meta.picture && <img src={meta.picture} alt="" />}
-                        {content}
+                    <div className="max-w-screen-sm mx-auto">
+                        <div className="flex justify-center mt-3 mb-9">
+                            <CategoryTag categoryName={meta.category} />
+                        </div>
+                        <div className="prose dark:prose-dark">
+                            <PostTitle meta={meta} />
+                            <p>{meta.excerpt}</p>
+                            {meta.picture && <img src={meta.picture} alt="" />}
+                            {content}
+                        </div>
                     </div>
                 </main>
                 <Footer />
